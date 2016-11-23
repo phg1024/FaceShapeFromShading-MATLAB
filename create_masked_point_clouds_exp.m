b2b13f40ad21f00a4378e0c5f7e738e59485b109
Peihong Guo <peihongguo@gmail.com>
2016-11-23 00:17:46 -0600

    Scripts for SFS with blendshapes.

---
+++ create_masked_point_clouds_exp.m
@@ -0,0 +1,56 @@
+clear; close all;
+
+person = 'Andy_Lau';
+path = sprintf('~/Storage/Data/InternetRecon2/%s/crop/', person);
+
+person = 'yaoming';
+path = sprintf('~/Storage/Data/InternetRecon0/%s/crop/', person);
+iteration_index = 1;
+
+all_images = read_settings(fullfile(path, 'settings.txt'));
+
+for i=1:length(all_images)
+    %close all;
+    
+    [~, basename, ext] = fileparts(all_images{i})
+    
+    input_image = fullfile(path, all_images{i});
+    
+    albedo_image = fullfile(path, ['iteration_', num2str(iteration_index)], 'SFS', sprintf('albedo_transferred_%d.png', i-1));
+    
+    d = load_depth_map(fullfile(path, ['iteration_', num2str(iteration_index)], 'SFS', sprintf('depth_map%d.bin', i-1)));
+    do = load_depth_map(fullfile(path, ['iteration_', num2str(iteration_index)], 'SFS', sprintf('optimized_depth_map_%d.bin', i-1)));
+    figure(1);imagesc(d(:,:,3));axis equal;
+    figure(2);imagesc(do(:,:,3));axis equal;
+    mask = do(:,:,3) < -10;
+    mask = 1 - mask;
+    
+    dorig = d(:,:,3) .* mask;
+    dopt = do(:,:,3) .* mask;
+    
+    figure(3);
+    subplot(1, 4, 1); imagesc(mask);axis equal; title('mask'); axis equal;
+    subplot(1, 4, 2); imagesc(dorig);axis equal; title('orig'); axis equal;
+    subplot(1, 4, 3); imagesc(dopt);axis equal; title('opt'); axis equal;
+    subplot(1, 4, 4); imagesc(abs(dorig - dopt));axis equal; title('diff'); axis equal; caxis([0 0.05]); colorbar;
+    
+    diff = abs(dorig - dopt);
+    diffimg = create_diff_image(diff, mask, 0, 0.15);
+    
+    figure(4); imshow(diffimg); title('diff'); axis equal;
+    
+    diffmask = diff < 0.05;
+    diffmask = diffmask .* mask;
+    figure(5); imshow(diffmask);
+    
+    masked_do = do;
+    masked_dopt = do(:,:,3);    
+    masked_dopt(diff >= 0.05) = -1e6;
+    %masked_dopt(~mask) = -1e6;
+    %figure;imagesc(masked_dopt);axis equal;title('masked depth');
+    masked_do(:,:,3) = masked_dopt;
+    
+    imwrite(diffimg, fullfile(path, ['iteration_', num2str(iteration_index)], 'SFS', sprintf('deformation_%d.png', i-1)));
+    imwrite(diffmask, fullfile(path, ['iteration_', num2str(iteration_index)], 'SFS', sprintf('deformation_mask_%d.png', i-1)));
+    save_point_cloud(fullfile(path, ['iteration_', num2str(iteration_index)], 'SFS', sprintf('masked_optimized_point_cloud_%d.txt', i-1)), masked_do);
+end
---
+++ driver_exp.m
@@ -0,0 +1,54 @@
+close all;
+
+person = 'Oprah_Winfrey';
+%person = 'Hillary_Clinton';
+%person = 'Benedict_Cumberbatch';
+%person = 'Donald_Trump';
+%person = 'George_W_Bush';
+%person = 'Zhang_Ziyi';
+%person = 'Andy_Lau';
+path = sprintf('/home/phg/Storage/Data/InternetRecon2/%s/crop', person);
+
+person = 'yaoming';
+path = sprintf('/home/phg/Storage/Data/InternetRecon0/%s/crop', person);
+iteration_index = 1;
+
+% LoG kernel and LoG matrix
+[LoG, mat_LoG] = LoGMatrix(2, 250, 250, 1.0);
+[albedo_LoG, albedo_mat_LoG] = LoGMatrix(2, 250, 250, 0.5);
+
+options.LoG = LoG;
+options.mat_LoG = mat_LoG;
+options.albedo_LoG = albedo_LoG;
+options.albedo_mat_LoG = albedo_mat_LoG;
+options.path = fullfile(path, ['iteration_', num2str(iteration_index)]);
+
+options.silent = true;
+
+all_images = read_settings(fullfile(path, 'settings.txt'));
+
+for i=1:length(all_images)
+input_image = fullfile(path, all_images{i});
+[~, basename, ~] = fileparts(all_images{i});
+albedo_image = fullfile(options.path, 'SFS', sprintf('albedo_transferred_%d.png', i-1));
+normal_image = fullfile(options.path, 'SFS', sprintf('normal%d.png', i-1));
+mask_image = fullfile(path, 'masked', sprintf('mask%s.png', basename));
+depth_map = fullfile(options.path, 'SFS', sprintf('depth_map%d.bin', i-1));
+
+options.idx = i-1;
+
+%[h, w, ~] = size(imread(albedo_image));
+%[LoG, mat_LoG] = LoGMatrix(2, h, w, 1.0);
+
+tic;
+refined_normal_map = SFS(input_image, albedo_image, normal_image, depth_map, mask_image, options);
+fprintf('image %d finished in %.3fs\n', i, toc);
+
+pause(2);
+end
+
+% TODO
+% restore image scaling in ioutilites
+% regenerate large scale
+% prepare for SFS
+% figure out how to generate mesh from SFS point cloud
